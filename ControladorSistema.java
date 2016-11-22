331da5ef0524eb90309897ebbff755aa8c2cfd90
jvazquez96 <jorge.armando@me.com>
2016-11-22 16:22:04 -0600

    Add validation when trying to register an existing user

--- ControladorSistema.java
+++ ControladorSistema.java
@@ -1,21 +1,24 @@
 import java.io.IOException;
+import java.util.*;
 
 public class ControladorSistema {
 
 	private ReadFile file = new ReadFile("archivo.txt");
 
-	public void readFile() throws IOException {
+	private String[] readFile() {
 		try {
 			String [] aryLines = this.file.OpenFile();
 			for (int i = 0; i < aryLines.length; ++i) {
-				System.out.println(aryLines[i]);
+				//System.out.println(aryLines[i]);
 			}
+			return aryLines;
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
+		return null;
 	}	
 	
-	public void writeFile(String Phrase) {
+	private void writeFile(String Phrase) {
 		try {
 			this.file.WriteFile(Phrase);
 		} catch (IOException e) {
@@ -26,8 +29,58 @@ public class ControladorSistema {
 	
 	public Usuario registro(String nombre, int edad, String correo, String contraseña, String tipo) {
 		Usuario nuevoUsuario = new Usuario(nombre,edad,correo,contraseña,tipo);
-		writeFile(nombre + " " + edad + " " + correo + " " + contraseña + " " + tipo);
+		if (!this.usuarioExistente(nuevoUsuario)){
+			writeFile(nombre + " " + edad + " " + correo + " " + contraseña + " " + tipo+"\n");
+		} 
 		return nuevoUsuario;
 	}
+
+	public ArrayList<Usuario> getUsuarios() {
+		ArrayList<Usuario> listOfUsers = new ArrayList<Usuario>();
+		String [] splittedString;
+		String nombre, correo, contraseña, tipo;
+		int edad;
+		String [] data = this.readFile();
+		for (int i = 0; i < data.length; ++i) {
+			Usuario nuevo = new Usuario();
+			splittedString = data[i].split(" ");
+			nombre = splittedString[0];
+			edad = Integer.parseInt(splittedString[1]);
+			correo = splittedString[2];
+			contraseña = splittedString[3];	
+			tipo = splittedString[4];
+			nuevo.setNombre(nombre);
+			nuevo.setEdad(edad);
+			nuevo.setCorreoElectronico(correo);
+			nuevo.setContraseña(contraseña);
+			nuevo.setTipo(tipo);
+			listOfUsers.add(nuevo);
+		}
+		return listOfUsers;
+	}	
+
+	public boolean usuarioExistente(Usuario usuario) {
+		String correoElectronico = usuario.getCorreoElectronico();
+		String contraseña = usuario.getContraseña();
+		String []  splittedString;
+		String nombre, correo, contra, tipo;
+		int edad;
+		String []  data = null;
+		data = this.readFile();
+		for (int i = 0; i < data.length; ++i) {
+			splittedString = data[i].split(" ");
+			nombre = splittedString[0];
+			edad = Integer.parseInt(splittedString[1]);
+			correo = splittedString[2];
+			contra = splittedString[3];
+			tipo = splittedString[4];
+			if (usuario.getCorreoElectronico().equals(correo) && usuario.getContraseña().equals(contra)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
 
 }
--- Sistema.java
+++ Sistema.java
@@ -7,15 +7,14 @@ public class Sistema {
 	private static ControladorSistema ContrSistema = new ControladorSistema();	
 	
 	public static void main (String[] args) {
+		readUsers();
 		adminLoggin();		
-		try {
-			System.out.println("Reading file......");
-			ContrSistema.readFile();	
-		} catch (IOException e) {
-			System.out.println(e.getMessage());	
-		}
 	}
 	
+
+	private static void readUsers() {
+		Usuarios = ContrSistema.getUsuarios();
+	}	
 	
 	private static void adminLoggin() {
 		System.out.println("Usuario");
@@ -30,7 +29,12 @@ public class Sistema {
 	}
 	
 	private static void userLoggin(String usuario, String contraseña) {
+		Usuario usuarioCheck = new Usuario();
+		usuarioCheck.setCorreoElectronico(usuario);
+		usuarioCheck.setContraseña(contraseña);
 		if (usuario.equals("usuario") && contraseña.equals("contraseña")) {
+			System.out.println("Admin logged in");
+		} else if (verify(usuarioCheck)) {
 			System.out.println("User logged in");
 		} else {
 			System.out.println("Asegurese de tener una cuenta o si desea registre una nueva cuenta");
@@ -40,17 +44,32 @@ public class Sistema {
 				System.out.println("Empezando registro..");
 				registro();
 			} else {
-				//TODO(jorge): Implemtar función para verificar con registro
 				System.out.println("Autentificación de usuario falló :(");
-				while (true) {
+				System.out.println("Usuario");
+				usuario = System.console().readLine();
+				System.out.println("Contraseña");
+				contraseña = System.console().readLine();
+				usuarioCheck.setCorreoElectronico(usuario);
+				usuarioCheck.setContraseña(contraseña);
+				while (!verify(usuarioCheck)) {
+					System.out.println("Autentificación de usuario falló :(");
 					System.out.println("Usuario");
 					usuario = System.console().readLine();
 					System.out.println("Contraseña");
 					contraseña = System.console().readLine();
-				}	
+					usuarioCheck.setCorreoElectronico(usuario);
+					usuarioCheck.setContraseña(contraseña);
+				} 
+				System.out.println("User logged in");
 			}
 		}	
 	}
+
+
+	private static boolean verify (Usuario usuarioVerificar) {
+		return ContrSistema.usuarioExistente(usuarioVerificar);
+	}
+
 	private static void registro() {
 		System.out.println("¿Cuál es tu nombre?");
 		Scanner in = new Scanner(System.in);
@@ -65,7 +84,9 @@ public class Sistema {
 		System.out.println("¿Qué tipo de usuario es usted? Primario(1)/Secundario(2)");
 		String tipo = System.console().readLine();
 		Usuario nuevoUsuario = ContrSistema.registro(nombre,edad,correo,contraseña,tipo);
-		Usuarios.add(nuevoUsuario);
-		System.out.println("Tamaño de lista: " + Usuarios.size());
+		if (!verify(nuevoUsuario)) {
+			System.out.println("Nuevo Usuario");
+			Usuarios.add(nuevoUsuario);
+		}				
 	}
 }
